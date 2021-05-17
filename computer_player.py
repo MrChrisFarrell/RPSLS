f193a29b78b8852f5c15bd68aed0b8e716811e49
unknown <mrchrisfarrell@yahoo.com>
2021-05-17 16:40:34 -0400

    player select now creates players and prints the match.

--- computer_player.py
+++ computer_player.py
@@ -5,7 +5,7 @@ from player import Player
 
 class ComputerPlayer(Player):
     def __init__(self):
-        super().__init__("Robot")
+        super().__init__("Kevin Costner")
 
     def select(self):
         self.choice = random.randint(0, len(self.choices) - 1)
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from player import Player
+from computer_player import ComputerPlayer
 
 
 class Game:
@@ -13,7 +14,7 @@ class Game:
         self.display_centered(self.welcome_message, 50)
         self.display_centered(f"This game will be best of {self.rounds}", 47)
         self.players_select()
-        print(self.players)
+
         #Have player(s) enter name(s)
         #player selects choice
         #compare choices
@@ -24,10 +25,17 @@ class Game:
         pass
 
     def players_select(self):
-        self.players = int(input("Enter the number of players (max 2)"))
+        self.players = int(input("\nEnter the number of players (max 2):"))
         while self.players <1 or self.players > 2:
             print("Invalid Input")
-            self.players = int(input("Enter the number of players (max 2)"))
+            self.players = int(input("Enter the number of players (max 2):"))
+        if self.players == 2:
+            self.player1.name = input("Enter Player-One name:")
+            self.player2.name = input("Enter Player-Two name:")
+        else:
+            self.player1.name = input("Enter Player-One name:")
+            self.player2 = ComputerPlayer()
+        print(f"\n{self.player1.name} is going against {self.player2.name}")
 
     def display_centered(self, message, display_width):
         print(message.center(display_width))
